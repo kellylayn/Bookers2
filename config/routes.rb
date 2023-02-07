200cc984202acdb94919d90907439f42f99cc091
kellylayn <kellyrabbit1@gmail.com>
2023-02-07 07:48:40 +0000

    [finish]ouyou4

--- config/routes.rb
+++ config/routes.rb
@@ -8,7 +8,11 @@ Rails.application.routes.draw do
   get '/home/about' => 'homes#about', as: 'about'
   # get 'users' => 'users#index', as: 'users'
   # resources :post_images,only: [:new, :create, :index, :show, :destroy]
-  resources :users,only: [:index, :show, :edit, :update]
+  resources :users,only: [:index, :show, :edit, :update] do
+    resource :relationships, only: [:create, :destroy]
+    get 'followings' => 'relationships#followings', as: 'followings'
+    get 'followers' => 'relationships#followers', as: 'followers'
+  end
   root :to => 'homes#top'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
